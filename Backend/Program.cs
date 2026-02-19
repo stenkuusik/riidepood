406a6a0b9db38bba4d31d06c8e77ec96fb26bb34
Sten Kuusik <stkuus@taltech.ee>
2026-02-19 20:52:58 +0200

    Initial setup completed.

---
+++ Backend/Program.cs
@@ -0,0 +1,24 @@
+using DotNetEnv;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
